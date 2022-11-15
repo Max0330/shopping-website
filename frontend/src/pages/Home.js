25256a22de6ea86f667547d7b5d4282b1ef89d7d
maxin0330 <59893911+maxin0330@users.noreply.github.com>
2022-11-14 18:24:02 -0800

    newest

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,14 +3,16 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
-import Footer from '../components/Footer'
+import Footer from '../components/footer'
+import Categories from '../components/Categories'
 export const Home = () => {
   return (
     <div>
         <Navbar />
         <Announcement />
         <Slider />
-        <Footer />
+        <Categories />
+        {/* <Footer /> */}
     </div>
   )
 }
